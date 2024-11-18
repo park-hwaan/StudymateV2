680fdc37bbd14bd33191c81556069d0ec635ec43
park-hwaan <parkhwan1129@gmail.com>
2024-11-18 18:43:01 +0900

    feat: 바텀네비게이션 클릭 이벤트 추가

--- app/src/main/java/com/example/studymatetwo/view/HomeActivity.kt
+++ app/src/main/java/com/example/studymatetwo/view/HomeActivity.kt
@@ -2,8 +2,13 @@ package com.example.studymatetwo.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.studymatetwo.R
 import com.example.studymatetwo.databinding.ActivityHomeBinding
+import com.example.studymatetwo.view.mainFragment.BoardFragment
+import com.example.studymatetwo.view.mainFragment.ChatFragment
+import com.example.studymatetwo.view.mainFragment.MentorSearchFragment
+import com.example.studymatetwo.view.mainFragment.MypageFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +20,33 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        loadFragment(MentorSearchFragment())
 
+        binding.bottomNav.setOnItemSelectedListener {
+            handleBottomNavigation(it.itemId)
+        }
+
+    }
+
+    private fun handleBottomNavigation(itemId: Int): Boolean {
+        val fragment = when (itemId) {
+            R.id.searchMentor -> MentorSearchFragment()
+            R.id.chat -> ChatFragment() // 각 메뉴에 맞는 프래그먼트로 수정
+            R.id.board -> BoardFragment()
+            R.id.myPage -> MypageFragment()
+            else -> null
+        }
+        return if (fragment != null) {
+            loadFragment(fragment)
+            true
+        } else {
+            false
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 }
