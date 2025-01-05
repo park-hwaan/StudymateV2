021fce0b3387520fb13e4089bb5c4bd72631766b
park-hwaan <parkhwan1129@gmail.com>
2025-01-05 20:27:21 +0900

    design: 게시판 디자인 수정

---
+++ app/src/main/java/com/example/studymatetwo/view/board/BoardWriteActivity.kt
@@ -0,0 +1,13 @@
+package com.example.studymatetwo.view.board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.studymatetwo.R
+
+class BoardWriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_write)
+
+    }
+}
